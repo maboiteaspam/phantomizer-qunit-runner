61595e8acfd0bd41c8275ba861e13d6940f3c3a7
clement <maboiteaspam@wanadoo.fr>
2014-02-03 16:23:16 +0800

    Code Style

--- tasks/build.js
+++ tasks/build.js
@@ -10,96 +10,96 @@ module.exports = function(grunt) {
     "Executes qunit tests in phantomjs", function () {
 
 
-    var webserver = ph_libutil.webserver;
-    var router_factory = ph_libutil.router;
-    var optimizer_factory = ph_libutil.optimizer;
-    var meta_factory = ph_libutil.meta;
+      var webserver = ph_libutil.webserver;
+      var router_factory = ph_libutil.router;
+      var optimizer_factory = ph_libutil.optimizer;
+      var meta_factory = ph_libutil.meta;
 
-    var grunt_config = grunt.config.get();
+      var grunt_config = grunt.config.get();
 
-    var options = this.options({
-      urls:[],
-      paths:[],
-      inject_assets:false,
-      base_url:"",
-      port:"",
-      ssl_port:"",
-      junitDir:null
-    });
-    grunt.verbose.writeflags(options, 'Options');
+      var options = this.options({
+        urls:[],
+        paths:[],
+        inject_assets:false,
+        base_url:"",
+        port:"",
+        ssl_port:"",
+        junitDir:null
+      });
+      grunt.verbose.writeflags(options, 'Options');
 
-    var q_options = {
-      all:{
-        options: {
-          force:true,
-          // always null, phantomizer handles it
-          inject:null,
-          urls: [],
-          junitDir:null
+      var q_options = {
+        all:{
+          options: {
+            force:true,
+            // always null, phantomizer handles it
+            inject:null,
+            urls: [],
+            junitDir:null
+          }
         }
-      }
-    };
+      };
 
-    var done = this.async();
+      var done = this.async();
 
-    var router = new router_factory(grunt_config.routing);
-    router.load(function(){
+      var router = new router_factory(grunt_config.routing);
+      router.load(function(){
 
-      var base_url = options.base_url;
-      if( base_url.substring(base_url.length-1) == "/" ){
-        base_url = base_url.substring(0, base_url.length-1)
-      }
-
-      grunt.log.ok("Building tests url")
-
-      var urls = [];
-      if( options.urls && options.urls.length > 0 ){
-        urls = collect_urls_from_options(options);
-      }else if ( grunt_config.routing ){
-        urls = collect_urls_from_config(grunt_config);
-      }
-
-      for( var url in urls ){
-        var tests = urls[url];
-        if( tests.length == 0 ){
-          grunt.log.warn("Missing tests for "+url)
-        }else{
-          tests = tests.join(",");
-          url = base_url+url;
-          url = url+(url.indexOf("?")>-1?"&":"?");
-          url = url+"spec_files="+tests;
-          //url = url+"&no_dashboard=true"; under windows the & makes bug
-          q_options.all.options.urls.push( url );
+        var base_url = options.base_url;
+        if( base_url.substring(base_url.length-1) == "/" ){
+          base_url = base_url.substring(0, base_url.length-1)
         }
-      }
 
-      if( q_options.all.options.urls.length > 0 ){
+        grunt.log.ok("Building tests url")
 
-        var meta_manager = new meta_factory(process.cwd(), grunt_config.meta_dir);
-        var optimizer = new optimizer_factory(meta_manager, grunt_config, grunt);
-        grunt_config.web_paths = options.paths;
-        webserver = new webserver(router,optimizer,meta_manager,process.cwd(), grunt_config, grunt);
-        webserver.is_phantom(true);
-        webserver.enable_dashboard(false);
-        webserver.enable_build(false);
-        webserver.enable_assets_inject(options.inject_assets);
-        webserver.start(options.port, options.ssl_port);
+        var urls = [];
+        if( options.urls && options.urls.length > 0 ){
+          urls = collect_urls_from_options(options);
+        }else if ( grunt_config.routing ){
+          urls = collect_urls_from_config(grunt_config);
+        }
 
-        grunt.registerTask('stop', 'Stop the webserver.', function() {
-          webserver.stop();
-        });
+        for( var url in urls ){
+          var tests = urls[url];
+          if( tests.length == 0 ){
+            grunt.log.warn("Missing tests for "+url)
+          }else{
+            tests = tests.join(",");
+            url = base_url+url;
+            url = url+(url.indexOf("?")>-1?"&":"?");
+            url = url+"spec_files="+tests;
+            //url = url+"&no_dashboard=true"; under windows the & makes bug
+            q_options.all.options.urls.push( url );
+          }
+        }
 
-        grunt.event.on('qunit.error.onError', function (message, stackTrace) {
-          if( stackTrace[0] && !stackTrace[0].file.match(/grunt-contrib-qunit\/phantomjs\/bridge[.]js$/))
-            grunt.log.ok("error.onError: " ,stackTrace);
-        });
+        if( q_options.all.options.urls.length > 0 ){
 
-        grunt.config.set("qunit", q_options);
-        grunt.task.run(["qunit","stop"])
-      }
-      done();
+          var meta_manager = new meta_factory(process.cwd(), grunt_config.meta_dir);
+          var optimizer = new optimizer_factory(meta_manager, grunt_config, grunt);
+          grunt_config.web_paths = options.paths;
+          webserver = new webserver(router,optimizer,meta_manager,process.cwd(), grunt_config, grunt);
+          webserver.is_phantom(true);
+          webserver.enable_dashboard(false);
+          webserver.enable_build(false);
+          webserver.enable_assets_inject(options.inject_assets);
+          webserver.start(options.port, options.ssl_port);
+
+          grunt.registerTask('stop', 'Stop the webserver.', function() {
+            webserver.stop();
+          });
+
+          grunt.event.on('qunit.error.onError', function (message, stackTrace) {
+            if( stackTrace[0] && !stackTrace[0].file.match(/grunt-contrib-qunit\/phantomjs\/bridge[.]js$/))
+              grunt.log.ok("error.onError: " ,stackTrace);
+          });
+
+          grunt.config.set("qunit", q_options);
+          grunt.task.run(["qunit","stop"])
+        }
+        done();
+      });
     });
-  });
 
   // helper functions
   // -----------
